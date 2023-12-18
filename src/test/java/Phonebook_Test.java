0cae9a5154392a15261fec95361aa6b93968b176
mahdieho-ohosseini <mahdieh.hosseini1378@gmail.com>
2023-12-18 18:18:04 +0330

    adding getPhoneNumber unitTest

--- src/test/java/Phonebook_Test.java
+++ src/test/java/Phonebook_Test.java
@@ -1,11 +1,12 @@
-/**
- * writing unit tests for each method of classes
- * If you have no idea about how to write unit tests, you can search the internet or check your first assignment
- * After each unit test you've written, please commit the changes and push to your repo
-
- * Hint: Your test coverage should be 100%
- * Attention:  We would check your commits and it will affect your score
- */
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 public class Phonebook_Test {
-
+    Person person1 = new Person("mahdieh","09115859055");
+    Phonebook phonebook = new Phonebook();
+    @Test
+    public void getPhoneNumber_Test(){
+        phonebook.addContact(person1);
+        assertEquals("09115859055", phonebook.getPhoneNumber("mahdieh"));
+        assertEquals("Contact Not Found", phonebook.getPhoneNumber(" "));
+    }
 }
