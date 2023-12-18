b5b20d2b3378f3eb2eee125e57343d246ab5488e
mahdieho-ohosseini <mahdieh.hosseini1378@gmail.com>
2023-12-18 18:33:17 +0330

    complite uniTest getcontact

--- src/test/java/Phonebook_Test.java
+++ src/test/java/Phonebook_Test.java
@@ -2,11 +2,24 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class Phonebook_Test {
     Person person1 = new Person("mahdieh","09115859055");
+    Person person2= new Person("zahra","09037800582");
     Phonebook phonebook = new Phonebook();
     @Test
     public void getPhoneNumber_Test(){
         phonebook.addContact(person1);
+        phonebook.addContact(person2);
         assertEquals("09115859055", phonebook.getPhoneNumber("mahdieh"));
+        assertEquals("09037800582",phonebook.getPhoneNumber("zahra"));
         assertEquals("Contact Not Found", phonebook.getPhoneNumber(" "));
+    }
+    @Test
+    public  void getContact(){
+       phonebook.addContact(person1);
+       phonebook.addContact(person2);
+       assertEquals(1,phonebook.getContact("mahdieh"));
+       assertEquals(1,phonebook.getContact("zahra"));
+       assertEquals(0,phonebook.getContact(" "));
+
+
     }
 }
