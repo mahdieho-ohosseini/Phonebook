60979d039a0c63eea72b372881e419201ec5eeac
mahdieho-ohosseini <mahdieh.hosseini1378@gmail.com>
2023-12-20 17:21:48 +0330

    edit two metod complit unitest updateContactPhoneNumber

--- src/test/java/Phonebook_Test.java
+++ src/test/java/Phonebook_Test.java
@@ -2,7 +2,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class Phonebook_Test {
     Person person1 = new Person("mahdieh","09115859055");
-    String h="hosi";
     Person person2= new Person("zahra","09037800582");
     Phonebook phonebook = new Phonebook();
     @Test
@@ -11,20 +10,24 @@ public class Phonebook_Test {
         phonebook.addContact(person2);
         assertEquals("09115859055", phonebook.getPhoneNumber("mahdieh"));
         assertEquals("09037800582",phonebook.getPhoneNumber("zahra"));
+        assertEquals("Contact Not Found", phonebook.getPhoneNumber("edxtrd"));
+        assertEquals("Contact Not Found", phonebook.getPhoneNumber(""));
         assertEquals("Contact Not Found", phonebook.getPhoneNumber(" "));
     }
     @Test
-    public  void getContact(){
+    public  void getContact_Test(){
        phonebook.addContact(person1);
        phonebook.addContact(person2);
        assertEquals(1,phonebook.getContact("mahdieh"));
        assertEquals(1,phonebook.getContact("zahra"));
+       assertEquals(0,phonebook.getContact("esryf"));
+       assertEquals(0,phonebook.getContact(""));
        assertEquals(0,phonebook.getContact(" "));
 
 
     }
     @Test
-    public  void updateContactName(){
+    public  void updateContactName_Test(){
         phonebook.addContact(person1);
         phonebook.addContact(person2);
         assertEquals(1,phonebook.updateContactName("mahdieh","maryam"));
@@ -34,6 +37,19 @@ public class Phonebook_Test {
         assertEquals(0,phonebook.updateContactName(" ","ali"));
 
     }
+    @Test
+    public  void updateContactPhoneNumber_Test(){
+
+        phonebook.addContact(person1);
+        phonebook.addContact(person2);
+        assertEquals(1,phonebook.updateContactPhoneNumber("mahdieh","09132149236"));
+        assertEquals(1,phonebook.updateContactPhoneNumber("zahra","09904849843"));
+        assertEquals(0,phonebook.updateContactPhoneNumber("njhjhn","09122159635"));
+        assertEquals(0,phonebook.updateContactPhoneNumber("","09122159635"));
+        assertEquals(0,phonebook.updateContactPhoneNumber(" ","09122159635"));
+
+    }
+
 
 
 
